bdc4982b87c567be7e258f9ffaa01bf6f6de12b0
Mayra Moreno <106444472+Mayra13p@users.noreply.github.com>
2022-10-01 12:07:10 -0500

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # config/initializers/assets.rb
-Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+
